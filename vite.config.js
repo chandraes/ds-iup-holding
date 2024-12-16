b58146b4fadae27f3ac85c21ec4040fa09bd0221
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-12-16 20:21:10 +0700

    Update package.json and vite.config.js to include vite-plugin-require and adjust commonjs plugin configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import commonjs from 'vite-plugin-commonjs';
-import vitePluginRequire from 'vite-plugin-require';
 
 export default defineConfig({
     plugins: [
@@ -9,7 +8,8 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        commonjs(),
-        vitePluginRequire(),
+        commonjs({
+            include: ['node_modules/preline/dist/*.js'],
+        }),
     ],
 });
