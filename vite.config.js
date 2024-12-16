c38f740ae277afd1aed1adf03e8fba691c1e1ee4
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-12-16 20:33:41 +0700

    Replace vite-plugin-commonjs with vite-plugin-require in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import commonjs from 'vite-plugin-commonjs';
 import vitePluginRequire from "vite-plugin-require";
 
 export default defineConfig({
@@ -9,9 +8,9 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        commonjs({
-            include: ['node_modules/preline/dist/*.js'],
+        vitePluginRequire.default({
+            // hasil build dari preline memanggil require tapi vite tidak mengenali require sehingga perlu di require ulang dengan vite-plugin-require
+            fileRegex: /(.jsx?|.tsx?|.vue|.js)$/,
         }),
-        vitePluginRequire.default(),
     ],
 });
